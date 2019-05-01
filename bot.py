e48e0367557de768199a975434822e2ac59e29d4
Nurbek Kuspanov <nurbek@inbox.ru>
2019-05-01 18:00:49 +0400

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,7 +1,16 @@
+import logging
 import os
 import requests
 import telegram
+import time
+import traceback
 
+class MyLogsHandler(logging.Handler):
+
+    def emit(self, record):
+        log_entry = self.format(record)  
+        bot.send_message(chat_id=chat_id, text=log_entry)
+        
 def get_data(url, headers, params=""):
   response = requests.get(url, headers=headers, timeout=30, params=params)
   return response.json()
@@ -15,18 +24,34 @@ if __name__ == '__main__':
     timestamp_parameter = ""
     telegram_token = os.environ["telegram_token"]
     chat_id = "335075762"
-    bot = telegram.Bot(token=telegram_token)
+    logger = logging.getLogger("Logger")
+    logger.setLevel(logging.INFO)
+    logger.addHandler(MyLogsHandler())
+    try:
+      bot = telegram.Bot(token=telegram_token)
+      logger.info('Бот запущен!')
+    except Exception:
+      logging.error('Бот не запущен!')
     while True:
       try:
         response = get_data(url, headers, params=timestamp_parameter)
         lesson_title = response["new_attempts"][0]["lesson_title"]
         lesson_url = dvmn_url + response["new_attempts"][0]["lesson_url"]
+        if response["new_attempts"][0]["is_negative"]:
+            lesson_pass = 'К сожалению, в работе нашлись ошибки.'
+        else:
+            lesson_pass = 'Преподавателю все понравилось, можно приступать к следующему уроку!'
         message = "Преподаватель проверил работу: "
-        text = "{message} \"{lesson_title}\" {lesson_url}".format(
+        text = "{message} \"{lesson_title}\" {lesson_url} {lesson_pass}".format(
               message=message,
               lesson_title=lesson_title,
-              lesson_url=lesson_url)
-        bot.send_message(chat_id=chat_id, text=text)
+              lesson_url=lesson_url,
+              lesson_pass=lesson_pass)      
+        logger.info(text)
         timestamp_parameter = str(response["last_attempt_timestamp"])
       except requests.exceptions.ReadTimeout:   
         pass
+      except Exception:
+        logger.info('Бот упал с ошибкой:')   
+        logger.info(traceback.format_exc())
+        time.sleep(30)      
