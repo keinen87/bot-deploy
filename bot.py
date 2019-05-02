79b8da5ecbad6a621780157ad0c9e2868e4ee6b4
Nurbek Kuspanov <nurbek@inbox.ru>
2019-05-03 00:04:37 +0400

    Update bot.py

--- bot.py
+++ bot.py
@@ -26,37 +26,40 @@ if __name__ == '__main__':
         logger.setLevel(logging.INFO)
         logger.addHandler(MyLogsHandler())
         logger.info('Бот запущен!')
-        while True:
-            try:
-                response = requests.get(longpolling_url, headers=headers, timeout=long_polling_timeout, params=timestamp_parameter)
-                if not response.ok:
-                    logger.error(f'Ответ от сайта: {response.text}')
-                    time.sleep(30)
-                    continue
-                response = response.json()
-                last_attempt = response["new_attempts"][0]
-                lesson_title = last_attempt["lesson_title"]
-                lesson_url = dvmn_url + last_attempt["lesson_url"]
-                if last_attempt["is_negative"]:
-                    lesson_pass = 'К сожалению, в работе нашлись ошибки.'
-                else:
-                    lesson_pass = 'Преподавателю все понравилось, можно приступать к следующему уроку!'
-                message = "Преподаватель проверил работу: "
-                text = "{message} \"{lesson_title}\" {lesson_url} {lesson_pass}".format(
-                        message=message,
-                        lesson_title=lesson_title,
-                        lesson_url=lesson_url,
-                        lesson_pass=lesson_pass)      
-                logger.info(text)
-                timestamp_parameter = str(response["last_attempt_timestamp"])
-            except requests.exceptions.ReadTimeout:   
-                pass
-            except requests.ConnectionError:
-                pass
-            except Exception:
-                logger.error('Бот упал с ошибкой:')   
-                logger.error(traceback.format_exc())
-                time.sleep(30)  
     except Exception:
         logging.error('Бот не запущен!')
         logger.error(traceback.format_exc())
+        exit()
+    while True:
+        try:
+            response = requests.get(longpolling_url, headers=headers, timeout=long_polling_timeout, params=timestamp_parameter)
+            if not response.ok:
+                logger.error(f'Ответ от сайта: {response.text}')
+                time.sleep(30)
+                continue
+            response = response.json()
+            last_attempt = response["new_attempts"][0]
+            lesson_title = last_attempt["lesson_title"]
+            lesson_url = dvmn_url + last_attempt["lesson_url"]
+            if last_attempt["is_negative"]:
+                lesson_pass = 'К сожалению, в работе нашлись ошибки.'
+            else:
+                lesson_pass = 'Преподавателю все понравилось, можно приступать к следующему уроку!'
+            message = "Преподаватель проверил работу: "
+            text = "{message} \"{lesson_title}\" {lesson_url} {lesson_pass}".format(
+                    message=message,
+                    lesson_title=lesson_title,
+                    lesson_url=lesson_url,
+                    lesson_pass=lesson_pass)      
+            logger.info(text)
+            timestamp_parameter = str(response["last_attempt_timestamp"])
+        except KeyError:
+            pass
+        except requests.exceptions.ReadTimeout:   
+            pass
+        except requests.ConnectionError:
+            pass
+        except Exception:
+            logger.error('Бот упал с ошибкой:')   
+            logger.error(traceback.format_exc())
+            time.sleep(30)
