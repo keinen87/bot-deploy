d6721f9c00d475c6f5d9035bb5467ed776098ee2
Nurbek Kuspanov <nurbek@inbox.ru>
2019-05-02 14:36:30 +0400

    Update bot.py

--- bot.py
+++ bot.py
@@ -6,52 +6,61 @@ import time
 import traceback
 
 class MyLogsHandler(logging.Handler):
-
+    def __init__(self, bot, chat_id):
+        self.bot = bot
+        self.chat_id = chat_id
+        
     def emit(self, record):
         log_entry = self.format(record)  
-        bot.send_message(chat_id=chat_id, text=log_entry)
-        
-def get_data(url, headers, params=""):
-  response = requests.get(url, headers=headers, timeout=30, params=params)
-  return response.json()
+        self.bot.send_message(chat_id=self.chat_id, text=log_entry)
 
 if __name__ == '__main__':
     dvmn_url = "https://dvmn.org"
     url = dvmn_url + "/api/long_polling/"
     headers = {
-      "Authorization": os.environ["dvmn_token"]
+      "Authorization": f'Token {os.environ["dvmn_token"]}'
     }
+    long_polling_timeout = 30
     timestamp_parameter = ""
     telegram_token = os.environ["telegram_token"]
-    chat_id = "335075762"
+    chat_id = os.environ["chat_id"]
     logger = logging.getLogger("Logger")
     logger.setLevel(logging.INFO)
     logger.addHandler(MyLogsHandler())
     try:
-      bot = telegram.Bot(token=telegram_token)
-      logger.info('Бот запущен!')
+        bot = telegram.Bot(token=telegram_token)
+        logger.info('Бот запущен!')
+        while True:
+            try:
+                response = requests.get(url, headers=headers, timeout=long_polling_timeout, params=timestamp_parameter)
+                if not response.ok:
+                    logger.error(f'Ответ от сайта: {response.text}')
+                    time.sleep(30)
+                    continue
+                else:
+                    response = response.json
+                    first_attempt = response["new_attempts"][0]
+                    lesson_title = first_attempt["lesson_title"]
+                    lesson_url = dvmn_url + first_attempt["lesson_url"]
+                    if first_attempt["is_negative"]:
+                        lesson_pass = 'К сожалению, в работе нашлись ошибки.'
+                    else:
+                        lesson_pass = 'Преподавателю все понравилось, можно приступать к следующему уроку!'
+                    message = "Преподаватель проверил работу: "
+                    text = "{message} \"{lesson_title}\" {lesson_url} {lesson_pass}".format(
+                            message=message,
+                            lesson_title=lesson_title,
+                            lesson_url=lesson_url,
+                            lesson_pass=lesson_pass)      
+                    logger.info(text)
+                    timestamp_parameter = str(response["last_attempt_timestamp"])
+            except requests.exceptions.ReadTimeout:   
+                pass
+            except requests.ConnectionError:
+                pass
+            except Exception:
+                logger.info('Бот упал с ошибкой:')   
+                logger.info(traceback.format_exc())
+                time.sleep(30)  
     except Exception:
-      logging.error('Бот не запущен!')
-    while True:
-      try:
-        response = get_data(url, headers, params=timestamp_parameter)
-        lesson_title = response["new_attempts"][0]["lesson_title"]
-        lesson_url = dvmn_url + response["new_attempts"][0]["lesson_url"]
-        if response["new_attempts"][0]["is_negative"]:
-            lesson_pass = 'К сожалению, в работе нашлись ошибки.'
-        else:
-            lesson_pass = 'Преподавателю все понравилось, можно приступать к следующему уроку!'
-        message = "Преподаватель проверил работу: "
-        text = "{message} \"{lesson_title}\" {lesson_url} {lesson_pass}".format(
-              message=message,
-              lesson_title=lesson_title,
-              lesson_url=lesson_url,
-              lesson_pass=lesson_pass)      
-        logger.info(text)
-        timestamp_parameter = str(response["last_attempt_timestamp"])
-      except requests.exceptions.ReadTimeout:   
-        pass
-      except Exception:
-        logger.info('Бот упал с ошибкой:')   
-        logger.info(traceback.format_exc())
-        time.sleep(30)      
+        logging.error('Бот не запущен!')
